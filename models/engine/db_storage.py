1bbc46ef49bda51b43ad0015179f08c8cc6f4df0
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-03-26 18:50:53 +0000

    add file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 """"""
-from models.base_model import BaseModel
+from models.base_model import Base
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from sqlalchemy import create_engine
+import os
 
 
+class DBStorage():
+    """"""
+    __engine = None
+    __session = None
 
+    def __init__(self):
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        database = os.getenv('HBNB_MYSQL_DB')
+        host = os.getenv('HBNB_MYSQL_HOST', 'localhost')
+        db_uri = f"mysql+mysqldb://{user}:{password}@{host}/{database}"
+        self.__engine = create_engine(db_uri, pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == 'test':
+            pass
