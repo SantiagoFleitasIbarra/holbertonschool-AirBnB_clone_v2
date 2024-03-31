ba89deec989fed6982da26dc9f0fe5af4febbfed
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2024-03-31 22:46:56 +0000

    update file

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
 
 class State(BaseModel, Base):
     """ State class """
